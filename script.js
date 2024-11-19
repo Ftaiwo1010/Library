0d1022c497294116ba4cd0657b9dcf5f342972c4
Oluwafemi <ftaiwo1010@gmail.com.noreply.github.com>
2024-11-19 14:47:30 +0100

    change Book function to class

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const library = [];
 
-function Book(title, author, pages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
+class Book {
+    constructor(title, author, pages, isRead) {
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+      this.isRead = isRead;
+    }
 }
-
-
 
 function addBookToLibrary(title, author, pages, isRead) {
     const newBook = new Book(title, author, pages, isRead);
